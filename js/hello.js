db4865d271004edb3d6753293b0c5921e1085c67
Matthias Kluth <matthias@kluth.me>
2012-03-05 16:06:33 +0100

    Added timing

--- js/hello.js
+++ js/hello.js
@@ -1,5 +1,9 @@
+var date = Date.now();
+console.log("Entry: " + date);
 var body = document.getElementsByTagName('body').item(0);
 var span = document.createElement('span');
 span.setAttribute('id', 'hellocontainer');
 span.appendChild(document.createTextNode('hello, world.'));
 body.appendChild(span);
+date = Date.now();
+console.log("Exit: " + date);
